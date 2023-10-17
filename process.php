2c76e85afa130498068b47d42d0bc511645cc660
jnm3383 <jnm3383@mavs.uta.edu>
2023-10-17 01:18:02 -0500

    form php

--- process.php
+++ process.php
@@ -2,11 +2,11 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
-    $reason = $_POST["reason"];
+    $subject = $_POST["subject"];
     $message = $_POST["message"];
 
     $to = "mccanlasj@gmail.com";
-    $subject = "Contact Form Submission - " . $reason;
+    $subject = "Contact Form Submission - " . $subject;
     $headers = "From: $email";
 
     mail($to, $subject, $message, $headers);
